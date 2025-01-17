9d0a61662df46dd6c9b069a98f3525bff0cc1727
Jahongir Hayitov <Jahongirhacking@gmail.com>
2025-01-17 21:00:42 +0500

    upd: change api url

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,6 +1,6 @@
 import { getLocalStorage, localStorageNames } from "./storageUtils";
 
-export const API_BASE_URL = "http://localhost:3000/api";
+export const API_BASE_URL = "/api";
 export const API_MAIN_URL = `${API_BASE_URL}/v1/design`;
 export const API_PROFILE_URL = `${API_BASE_URL}/v1/profile`;
 
